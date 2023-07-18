dc09e1ce65bc0cc0321cb8f2353513bb772c26f0
장진욱 <>
2023-07-18 10:08:01 +0900

    test

---
+++ back_idiots/back_idiots/src/main/java/com/example/back_idiots/BackIdiotsApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_idiots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackIdiotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackIdiotsApplication.class, args);
+	}
+
+}
---
+++ back_idiots/back_idiots/src/test/java/com/example/back_idiots/BackIdiotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_idiots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackIdiotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
