dc9f6292f8291244d2626e32785e9c8e583875d7
KimHyeongJin <gudwls9966@naver.com>
2023-07-21 14:50:52 +0900

    setting

--- backend/jinwook/src/main/java/com/backend/board/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.board.repository;
-
-import com.backend.board.model.MemberDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<MemberDTO, Long> {
-    MemberDTO findByUsername(String username);
-}
