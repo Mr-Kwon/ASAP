bc9b7166733dffd57585a31a27006b5ecc310c52
kieanu <kieanupark@gmail.com>
2023-07-22 22:23:06 +0900

    feat: 자리 배치 프래그먼트 개발 중

--- frontend/ASAF/app/src/main/java/com/d103/asaf/ui/op/OpFragmentViewModel.kt
+++ frontend/ASAF/app/src/main/java/com/d103/asaf/ui/op/OpFragmentViewModel.kt
@@ -1,7 +1,23 @@
 package com.d103.asaf.ui.op
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class OpFragmentViewModel: ViewModel() {
-
+//    private val _rooms: MutableStateFlow<List<Room>> = MutableStateFlow(emptyList())
+//    val rooms = _rooms
+//
+//    init {
+//        loadRooms()
+//    }
+//
+//    private fun loadRooms() {
+//        viewModelScope.launch {
+//            ApplicationClass.fireStore.getRoom().collect() {
+//                _rooms.emit(it)
+//            }
+//        }
+//    }
 }
---
+++ frontend/ASAF/app/src/main/java/com/d103/asaf/ui/op/SeatFragment.kt
@@ -0,0 +1,9 @@
+package com.d103.asaf.ui.op
+
+import com.d103.asaf.R
+import com.d103.asaf.common.config.BaseFragment
+import com.d103.asaf.databinding.FragmentSeatBinding
+
+class SeatFragment : BaseFragment<FragmentSeatBinding>(FragmentSeatBinding::bind, R.layout.fragment_seat) {
+
+}
