e20cd7dc5051170d26011b720b31e14df4eb9f2f
Kim hyeongjin <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-07-23 17:37:52 +0900

    feat(형진) : 속성에서 데이터베이스 member에서 ASAF으로 변경

--- backend/ASAF/src/main/java/com/ASAF/entity/AttendanceEntity.java
+++ backend/ASAF/src/main/java/com/ASAF/entity/AttendanceEntity.java
@@ -1,6 +1,8 @@
 package com.ASAF.entity;
 
+import com.ASAF.dto.AttendanceDTO;
 import javax.persistence.*;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +27,23 @@ public class AttendanceEntity {
 
     @Column
     private String exitTime;
+
+    public static AttendanceEntity toAttendanceEntity(AttendanceDTO attendanceDTO) {
+        AttendanceEntity attendanceEntity = new AttendanceEntity();
+        attendanceEntity.setAttended(attendanceDTO.getAttended());
+        attendanceEntity.setEntryTime(attendanceDTO.getEntryTime());
+        attendanceEntity.setExitTime(attendanceDTO.getExitTime());
+        // 멤버 엔티티 세팅은 더 정교한 매핑이 필요하므로, 별도로 추가하세요.
+        return attendanceEntity;
+    }
+
+    public static AttendanceEntity toUpdateAttendanceEntity(AttendanceDTO attendanceDTO) {
+        AttendanceEntity attendanceEntity = new AttendanceEntity();
+        attendanceEntity.setAttendanceId(attendanceDTO.getAttendanceId());
+        attendanceEntity.setAttended(attendanceDTO.getAttended());
+        attendanceEntity.setEntryTime(attendanceDTO.getEntryTime());
+        attendanceEntity.setExitTime(attendanceDTO.getExitTime());
+        // 멤버 엔티티 세팅은 더 정교한 매핑이 필요하므로, 별도로 추가하세요.
+        return attendanceEntity;
+    }
 }
