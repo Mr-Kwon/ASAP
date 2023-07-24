57e89aa8e7226dad6ca967f76bc001c5510264c5
Kim hyeongjin <127001416+OneDayOneAlgorithm@users.noreply.github.com>
2023-07-24 23:15:18 +0900

    feat(형진) : 기능 구현중

--- backend/ASAF/src/main/java/com/ASAF/controller/AttendanceController.java
+++ backend/ASAF/src/main/java/com/ASAF/controller/AttendanceController.java
@@ -1,26 +1,34 @@
+// AttendanceController.java
 package com.ASAF.controller;
 
 import com.ASAF.dto.AttendanceDTO;
 import com.ASAF.service.AttendanceService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequiredArgsConstructor
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/attendance")
 public class AttendanceController {
-    private final AttendanceService attendanceService;
 
-    // 컨트롤러
-    @PostMapping("/attendance")
-    public ResponseEntity<String> attendance(@RequestParam("student_number") String student_number) {
-        String resultMessage = attendanceService.check_in(student_number);
-        return new ResponseEntity<>(resultMessage, HttpStatus.OK);
+    @Autowired
+    private AttendanceService attendanceService;
+
+
+
+//    @PostMapping("/checkin/{id}")
+//    public AttendanceDTO checkIn(@PathVariable int id) {
+//        return attendanceService.updateAttendanceByMemberId(id);
+//    }
+//
+//    @PostMapping("/checkout/{memberId}")
+//    public AttendanceDTO checkOut(@PathVariable int memberId) {
+//        return AttendanceDTO.toAttendanceDTO(attendanceService.attendanceCheckOut(memberId));
+//    }
+
+    @GetMapping
+    public List<AttendanceDTO> getAllAttendances() {
+        return attendanceService.findAll();
     }
-
 }
