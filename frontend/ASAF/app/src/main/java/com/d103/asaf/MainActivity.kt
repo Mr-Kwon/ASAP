0763b835c0779b8a22d7d902ba95ac18db97eebf
DESKTOP-DOGVPUB\SSAFY <know0329@pusan.ac.kr>
2023-07-24 13:22:59 +0900

    feat : fragment 추가
    폴더 별 fragment 추가

--- frontend/ASAF/app/src/main/java/com/d103/asaf/MainActivity.kt
+++ frontend/ASAF/app/src/main/java/com/d103/asaf/MainActivity.kt
@@ -3,9 +3,15 @@ package com.d103.asaf
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.d103.asaf.common.config.BaseActivity
 import com.d103.asaf.common.model.dto.Accounts_user
 import com.d103.asaf.databinding.ActivityMainBinding
+import com.tbuonomo.morphbottomnavigation.MorphBottomNavigationView
 import java.sql.Date
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
@@ -13,12 +19,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     private lateinit var user : Accounts_user
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
-
+        setSupportActionBar(findViewById(com.airbnb.lottie.R.id.action_bar));
         // user 정보 가지고 오기
         // 임시 데이터
         user = Accounts_user(2, "Test", "Test@test.com", "test",
             Date(System.currentTimeMillis()) ,
-            "010-1234-5678", "test", "123", "1","학생")
+            "010-1234-5678", "test", "123", "1","프로")
         // user 정보 가지고 오고 난 후 activity 나누기
 
         when(user.authority){
@@ -29,7 +35,26 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             "프로" -> {
                 binding.bottomNaviPro.visibility = View.VISIBLE
                 binding.bottomNaviStudent.visibility = View.GONE
+
+
+
+//                val navView: MorphBottomNavigationView =  binding.bottomNaviPro
+//
+//                val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//                // Passing each menu ID as a set of Ids because each
+//                // menu should be considered as top level destinations.
+//                val appBarConfiguration = AppBarConfiguration(setOf(
+//                    R.id.navigation_op, R.id.navigation_schedule, R.id.navigation_pro_home, R.id.navigation_library_management, R.id.navigation_bus))
+//                setupActionBarWithNavController(navController, appBarConfiguration)
+//                navView.setupWithNavController(navController)
+
+
+
+
             }
         }
+
+
+
     }
 }
--- frontend/ASAF/app/src/main/java/com/d103/asaf/ui/bus/BusFragment.kt
+++ frontend/ASAF/app/src/main/java/com/d103/asaf/ui/bus/BusFragment.kt
@@ -1,12 +1,11 @@
-package com.d103.asaf.ui.home.pro
+package com.d103.asaf.ui.bus
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.d103.asaf.R
-import com.d103.asaf.common.component.CustomToggleButton
-import com.d103.asaf.common.config.BaseFragment
-import com.d103.asaf.databinding.FragmentProHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,14 +14,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ProHomeFragment.newInstance] factory method to
+ * Use the [BusFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProHomeFragment : BaseFragment<FragmentProHomeBinding>(FragmentProHomeBinding::bind, R.layout.fragment_pro_home) {
+class BusFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var toggleButton : CustomToggleButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -31,10 +30,14 @@ class ProHomeFragment : BaseFragment<FragmentProHomeBinding>(FragmentProHomeBind
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        toggleButton = CustomToggleButton(requireContext())
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bus, container, false)
     }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -42,12 +45,12 @@ class ProHomeFragment : BaseFragment<FragmentProHomeBinding>(FragmentProHomeBind
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ProHomeFragment.
+         * @return A new instance of fragment BusFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ProHomeFragment().apply {
+            BusFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
