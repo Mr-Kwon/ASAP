0c28e9fb0d2a48d905d312628cc8492df40f1973
Kwon_Min_Jae <knk598500@gmail.com>
2023-07-25 13:27:52 +0900

    fix : 권민재 문서관리 기능 구현및 검사 완료 DocumentDTO 수정 DocumentEntity 수정 DocumentController 생성 DocumentService 생성

--- backend/ASAF/src/main/java/com/ASAF/dto/DocumentDTO.java
+++ backend/ASAF/src/main/java/com/ASAF/dto/DocumentDTO.java
@@ -1,6 +1,5 @@
 package com.ASAF.dto;
 
-import com.ASAF.entity.ClassInfoEntity;
 import com.ASAF.entity.DocumentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,16 +12,24 @@ import lombok.ToString;
 @ToString
 public class DocumentDTO {
     private Long doc_id;
-    private ClassInfoDTO classInfoDTO;
+    private int class_num;
+    private int class_code;
+    private int region_code;
+    private int generation_code;
+    private String user_id;
     private String doc_style;
 
+
+    public DocumentDTO(DocumentEntity documentEntity) {
+        this.doc_id = documentEntity.getDoc_id();
+        this.doc_style = documentEntity.getDoc_style();
+    }
     // DocumentEntity 타입의 하나의 매개변수 documentEntity를 입력으로 받고 DocumentDTO 객체를 반환합니다.
     // 이 메서드의 목적은 DocumentEntity 객체의 정보를 가져와 DocumentDTO 객체로 변환하는 것입니다.
     public static DocumentDTO toDocumentDTO(DocumentEntity documentEntity) {
         DocumentDTO documentDTO = new DocumentDTO();
         documentDTO.setDoc_id(documentEntity.getDoc_id());
         // ClassInfoEntity를 ClassInfoDTO로 매핑
-        documentDTO.setClassInfoDTO(ClassInfoDTO.toClassInfoDTO(documentEntity.getClass_num()));
         documentDTO.setDoc_style(documentEntity.getDoc_style());
         return documentDTO;
     }
