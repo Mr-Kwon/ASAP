1a1e4caceee5b2898fcd72a2b3957820bb7f7b6f
김준성 <know0329@pusan.ac.kr>
2023-07-31 10:28:23 +0900

    feat : SharedVIewModel에 반 정보 받아오는 코드 추가

--- frontend/ASAF/app/src/main/java/com/d103/asaf/SharedViewModel.kt
+++ frontend/ASAF/app/src/main/java/com/d103/asaf/SharedViewModel.kt
@@ -1,7 +1,49 @@
 package com.d103.asaf
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.d103.asaf.common.model.dto.Classinfo
+import com.d103.asaf.common.model.dto.Member
+import com.d103.asaf.common.util.RetrofitUtil
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
+private const val TAG = "SharedViewModel ASAF"
 class SharedViewModel : ViewModel() {
-     lateinit var selectedDate : String
+     
+     
+     lateinit var selectedDate : String // 캘린더에서 쓰는 데이터
+     
+     private val _classInfoList = MutableLiveData<MutableList<Classinfo>>()
+     val classInfoList : LiveData<MutableList<Classinfo>>
+          get() = _classInfoList
+
+     fun getClassInfo(user : Member){
+          viewModelScope.launch {
+
+               try {
+                    val response = RetrofitUtil.attendenceService.getClassInfo(user.id)
+                    if(response.isSuccessful){
+                         val responseBody = response.body()
+                         if(!responseBody.isNullOrEmpty()){
+                              _classInfoList.postValue(responseBody!!)
+                         }
+                         else{
+
+                              Log.d(TAG, "통신 ERROR : responseBody가 NULL")
+                         }
+
+
+                    }
+
+               } catch (e : Exception){
+                    Log.d(TAG, "통신 에러: ${e.printStackTrace()}")
+               }
+
+          }
+     }
+     
 }
