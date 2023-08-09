fd140dea04b0dbba20c7929ea2d7d8632e1ed95c
KimHyeongJin <gudwls9966@naver.com>
2023-08-09 14:12:04 +0900

    젠킨스 푸쉬

--- backend/ASAF/src/main/java/com/ASAF/config/DataLoader.java
+++ backend/ASAF/src/main/java/com/ASAF/config/DataLoader.java
@@ -4,6 +4,7 @@ master branch로 push 시 수정해야 할 것
 1-2. 더미데이터 사용할 땐 이미지 경로 변경
 2. memberService 이미지 save하는 메서드 경로 변경
 3. property create에서 update로 변경
+4. FirebaseCloudMessagingInitializer에서 String jsonPath = "/home/ubuntu/ASAF_FCM_KEY.json"; 로 변경
  */
 
 
