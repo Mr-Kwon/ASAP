d459788389eb1585e32a19b6e312a3cade08bece
KimHyeongJin <gudwls9966@naver.com>
2023-08-14 16:41:44 +0900

    fcm

--- backend/ASAF/src/main/java/com/ASAF/config/FirebaseCloudMessagingInitializer.java
+++ backend/ASAF/src/main/java/com/ASAF/config/FirebaseCloudMessagingInitializer.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class FirebaseCloudMessagingInitializer {
     public void initializeFirebaseApp() throws IOException {
         // 윈도우 경로를 사용하므로, 경로를 올바른 형태로 변경하였습니다.
-        String jsonPath = "C:\\Users\\SSAFY\\Desktop\\S09P12D103\\backend\\ASAF\\src\\main\\resources\\ASAF_FCM_KEY.json";
-//        String jsonPath = "/home/ubuntu/ASAF_FCM_KEY.json";
+//        String jsonPath = "C:\\Users\\SSAFY\\Desktop\\S09P12D103\\backend\\ASAF\\src\\main\\resources\\ASAF_FCM_KEY.json";
+        String jsonPath = "/home/ubuntu/ASAF_FCM_KEY.json";
 
 
         FirebaseOptions options = new FirebaseOptions.Builder()
